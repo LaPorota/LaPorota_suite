d6d90883f5af529fe301dea66a3672514f3ee2e2
PedroOrlando <101206218+LaPorota@users.noreply.github.com>
2024-09-19 08:49:25 -0300

    Update nmap_scan.bash

--- infraestructura/nmap_scan.bash
+++ infraestructura/nmap_scan.bash
@@ -2,3 +2,7 @@ for i in {rango de puertos}; do; echo "--------$i-------"; sudo nmap -sV -p $i i
 
 
 21,22,23,25,53,80,111,139,445,512,513,514,1099,1524,2049,2121,3306,3632,5432,5900,6000,6667,6697,8009,8180,8787,35459,38644,40354,60266
+
+Printear todo junto luego
+
+for file in *; do [ -f "$file" ] && echo "Mostrando contenido de: $file" && cat "$file" && echo -e "\n------------------------\n"; done
